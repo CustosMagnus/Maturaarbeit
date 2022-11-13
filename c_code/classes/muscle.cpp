17b04c1d9b465ea2080a479889f2ddc36cb94b8c
CustosMagnus <mcarle@rsnweb.ch>
2022-11-13 19:24:22 +0100

    update

--- c_code/classes/muscle.cpp
+++ c_code/classes/muscle.cpp
@@ -10,13 +10,12 @@ void muscle::init(double angle_gamma, double length_u) {
 double muscle::calc_F(double F_N, double zeta) {
     // F = (6.9e1/5.90e2*F_N); // <-- f(x)
     F = get_gx(F_N);
-    if (F<0){F=0;}
     return (F*sin(omega)*u)/(sin(zeta)*l);
 }
 
 double muscle::get_gx(double F_N) {
     double peroneus_F_N, zeta, omega_, F_peroneus_2, F_N_2, F_N_3, t;
-    t = 9e-1;
+    t = 6e-1;
     peroneus_F_N = 140 * t;
     zeta = rad(63);
     omega_ = rad(45);
@@ -25,6 +24,7 @@ double muscle::get_gx(double F_N) {
     F_N_3 = 1551.89;
     const double a = ((peroneus_F_N*sin(zeta)*l)/(sin(omega_)*u)-F_peroneus_2*(F_N_3/F_N_2))/(pow(F_N_3,2)-pow(F_N_2,2)*(F_N_3/F_N_2));
     const double b = (F_peroneus_2-a*pow(F_N_2,2))/F_N_2;
-    const double c = 0;
+    const double c = pow(b, 2)/(4*a); // c is x at g'(x) = 0
+    cout<<a<<endl<<b<<endl<<c<<endl;
     return (a*pow(F_N, 2)+b*F_N+c);
 }
