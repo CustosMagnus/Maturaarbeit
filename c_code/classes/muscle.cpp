d140305b6f6eb19e95bc7b70f0be2d10de6bef5f
CustosMagnus <mcarle@rsnweb.ch>
2022-11-14 23:33:14 +0100

    added both muscles

--- c_code/classes/muscle.cpp
+++ c_code/classes/muscle.cpp
@@ -8,13 +8,16 @@ void muscle::init(double angle_gamma, double length_u, string muscle_name_) {
 }
 
 double muscle::calc_F(double F_N, double zeta) {
-    // F_ligament = (6.9e1/5.90e2*F_N); // <-- f(x)
     F = get_gx(F_N);
     return (F*sin(omega)*u)/(sin(zeta)*l);
 }
 
 double muscle::get_gx(double F_N) {
     if (muscle_name == "peroneus") {
+        // ax+b=y
+        // return (6.9e1/5.90e2*F_N); // <-- f(x)
+
+        // ax^2+bx+c=y
         double peroneus_F_N, zeta, omega_, F_peroneus_2, F_N_2, F_N_3, t;
         t = 6e-1;
         peroneus_F_N = 140 * t;
@@ -27,6 +30,14 @@ double muscle::get_gx(double F_N) {
                          (pow(F_N_3, 2) - pow(F_N_2, 2) * (F_N_3 / F_N_2));
         const double b = (F_peroneus_2 - a * pow(F_N_2, 2)) / F_N_2;
         const double c = pow(b, 2) / (4 * a); // c is x at g'(x) = 0
-        return (a * pow(F_N, 2) + b * F_N + c);
+        return (a * pow(F_N, 2) + b * F_N + c); // <-- g(x)
+    }
+    else if (muscle_name == "flexor_hallucis_longus"){
+        // ax+b=y
+        return (4.3e1/2.95e2*F_N); // <-- f(x)
+    }
+    else if (muscle_name == "flexor_digitorum_longus"){
+        // ax+b=y
+        return (1.4e1/2.95e2*F_N); // <-- f(x)
     }
 }
